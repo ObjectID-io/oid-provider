ab6998b48a8efdb83c161605a6673bf6ccd7edea
sdellava <sdellava@gmail.com>
2026-02-05 11:48:58 +0100

    fix default

--- src/onchain/defaults.ts
+++ src/onchain/defaults.ts
@@ -9,7 +9,7 @@ export type CanonicalNetwork = "testnet" | "mainnet";
  */
 export const DEFAULT_CONFIG_PACKAGE_IDS: ConfigPackageIds = {
   testnet: "0x7560b5cb2024a3a712ed1e09f4e42ba806b886a44262c9d40ac31fbcfeb30cc0",
-  mainnet: "", // TODO: set mainnet config package id
+  mainnet: "0x6148d3674590634d59f8f972d71dd6148e014dd9c036c048d446853dd80049f4",
 };
 
 /**
@@ -18,5 +18,5 @@ export const DEFAULT_CONFIG_PACKAGE_IDS: ConfigPackageIds = {
  */
 export const DEFAULT_SHARED_CONFIG_OBJECT_ID: Record<string, string> = {
   testnet: "0xf9d3f786ac5ee53f293b87f6800eee852d6e7263d78275377f5d13db04f4be15",
-  mainnet: "",
+  mainnet: "0x3351d3692783c1b569d6ebe8b0ede46dec51e5688b2892d20b32c0a94414043d",
 };
